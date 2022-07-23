ab9afe5dcfac4f8bf6c13544c15a65f503dfb0f0
Anson <ansfoong@gmail.com>
2022-07-23 03:02:13 +0200

    Application Commands

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import { config } from 'dotenv';
+import { Client, GatewayIntentBits, Routes } from 'discord.js';
+import { REST } from '@discordjs/rest';
+
+config();
+
+const TOKEN = process.env.TUTORIAL_BOT_TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+client.on('ready', () => console.log(`${client.user.tag} has logged in!`));
+
+client.on('interactionCreate', (interaction) => {
+  if (interaction.isChatInputCommand()) {
+    console.log('Hello, World');
+    interaction.reply({ content: 'Hey there!!!!!!' });
+  }
+});
+
+async function main() {
+  const commands = [
+    {
+      name: 'order',
+      description: 'Order something...',
+    },
+  ];
+  try {
+    console.log('Started refreshing application (/) commands.');
+    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+      body: commands,
+    });
+    client.login(TOKEN);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+main();
